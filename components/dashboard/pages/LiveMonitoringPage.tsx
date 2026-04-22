9b125e43344de49fa7b5a14c46de557c2faf0c41
Waleed Shoaib <87224462+waleedshoaib2@users.noreply.github.com>
2026-04-23 01:50:48 +0500

    live monitoring updated

--- components/dashboard/pages/LiveMonitoringPage.tsx
+++ components/dashboard/pages/LiveMonitoringPage.tsx
@@ -10,7 +10,7 @@ const MONITORING_TOOLS = [
   },
   {
     name: "CloudTalk",
-    url: "https://phone.cloudtalk.io?partner=insurvas",
+    url: "https://analytics.cloudtalk.io/",
   },
 ] as const;
 
